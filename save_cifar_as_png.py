96e088cea88eaee55382ef8c59f7acc2fc27f02f
Berkay <c.berkayantmen@gmail.com>
2017-06-18 21:15:52 -0400

    Refactored a bit

--- save_cifar_as_png.py
+++ save_cifar_as_png.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-reload(sys)
 sys.setdefaultencoding('utf8')
 
 def unpickle(f):
@@ -13,34 +12,26 @@ def unpickle(f):
         dct = pickle.load(fo)
     return dct
 
+def save_dirs_as_png(root, in_dirs, out_dir):
+    for i, unpack in enumerate(in_dirs):
+        print i
+        path = os.path.join(root, unpack)
+        datasets = unpickle(path)
+        for j in xrange(datasets['data'].shape[0]):
+            im = datasets['data'][j,:].reshape((32,32,3), order='F').T
+            im = np.rot90(im.T, k=3)
+            label = datasets['labels'][j]
+            scipy.misc.imsave('{}/{}/{}.png'.format(out_dir, label, 10000*i + j), im)
 
 if __name__ == "__main__":
-    file = "/Users/cemanil/Projects/Watermark/cifar-10-batches-py"
+    root = "cifar-10-batches-py"
+
     trains = ['data_batch_1',
               'data_batch_2',
               'data_batch_3',
               'data_batch_4',
               'data_batch_5']
+    save_dirs_as_png(root, trains, 'cifar_png/training')
 
     tests = ['test_batch']
-
-    for i, unpack in enumerate(trains):
-        print i
-        path = os.path.join(file, unpack)
-        datasets = unpickle(path)
-        for j in range(datasets['data'].shape[0]):
-            im = datasets['data'][j,:].reshape((32,32,3), order='F').T
-            im = np.rot90(im.T, k=3)
-            label = datasets['labels'][j]
-            scipy.misc.imsave('cifar_png/training/{}/{}.png'.format(label, 10000*i + j), im)
-
-    for i, unpack in enumerate(tests):
-        print i
-        path = os.path.join(file, unpack)
-        datasets = unpickle(path)
-        for j in range(datasets['data'].shape[0]):
-            im = datasets['data'][j,:].reshape((32,32,3), order='F').T
-            im = np.rot90(im.T, k=3)
-            label = datasets['labels'][j]
-            scipy.misc.imsave('cifar_png/testing/{}/{}.png'.format(label, 50000 + j), im)
-
+    save_dirs_as_png(root, tests, 'cifar_png/testing')
